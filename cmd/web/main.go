c9dc399f33008642cea5728cd39d91eab69ae4af
Gleb Sharapov <glebdeshar@gmail.com>
2024-04-06 07:48:01 +0300

    Create ws endpoint and basic html scetch

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,5 +1,12 @@
 package main
 
-func main() {
+import (
+	"log"
+	"net/http"
+)
 
+func main() {
+	mux := routes()
+	log.Println("Start server")
+	_ = http.ListenAndServe(":8080", mux)
 }
--- internal/handlers/handlers.go
+++ internal/handlers/handlers.go
@@ -1,7 +1,68 @@
 package handlers
 
-import "net/http"
+import (
+	"log"
+	"net/http"
+
+	"github.com/CloudyKit/jet/v6"
+	"github.com/gorilla/websocket"
+)
+
+var views = jet.NewSet(
+	jet.NewOSFileSystemLoader("D:/Programming/go/WebSocket/html"),
+	jet.InDevelopmentMode(),
+)
+
+var upgradeConnection = websocket.Upgrader{
+	ReadBufferSize:  1024,
+	WriteBufferSize: 1024,
+	CheckOrigin:     func(r *http.Request) bool { return true },
+}
 
 func Home(w http.ResponseWriter, r *http.Request) {
+	err := renderPage(w, "home.html", nil)
+	if err != nil {
+		log.Println(err)
+	}
 
+}
+
+type WsJsonResponse struct {
+	Action      string `json:"action"`
+	Message     string `json:"Message"`
+	MessageType string `json:"MessageType"`
+}
+
+func WsEndPoint(w http.ResponseWriter, r *http.Request) {
+
+	ws, err := upgradeConnection.Upgrade(w, r, nil)
+	if err != nil {
+		log.Println(err)
+	}
+
+	log.Println("Client connected to endpoint")
+
+	var response WsJsonResponse
+	response.Message = `<em><small>Connected to server</small></em>`
+
+	err = ws.WriteJSON(response)
+	if err != nil {
+		log.Println(err)
+	}
+}
+
+func renderPage(w http.ResponseWriter, tmpl string, data jet.VarMap) error {
+	view, err := views.GetTemplate(tmpl)
+	if err != nil {
+		log.Panicln(err)
+		return err
+	}
+
+	err = view.Execute(w, data, nil)
+	if err != nil {
+		log.Panicln(err)
+		return err
+	}
+
+	return nil
 }
